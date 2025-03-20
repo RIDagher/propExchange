ac5d9d5ebd4ef6dce3726cfc94b5046d5e6d8611
David Demers <daviddemers92@gmail.com>
2025-03-19 20:40:34 -0400

    Migrations and users model created

---
+++ database/migrations/2025_03_19_201415_create_properties_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->id('propertyId');
+            $table->string('title');
+            $table->text('description');
+            $table->decimal('price', 10, 2);
+            $table->string('address');
+            $table->string('city');
+            $table->string('province');
+            $table->string('postalCode');
+            $table->unsignedBigInteger('agentId');
+            $table->boolean('isSold')->default(false);
+            $table->enum('propertyType', ['House', 'Condo', 'Cottage', 'Multiplex']);
+            $table->integer('floors');
+            $table->integer('bedrooms');
+            $table->integer('bathrooms');
+            $table->decimal('squareFootage', 10, 2);
+            $table->integer('yearBuilt');
+            $table->boolean('isGarage')->default(false);
+            $table->timestamps();
+
+            $table->foreign('agentId')->references('userId')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('properties');
+    }
+};
---
+++ database/migrations/2025_03_19_201431_create_property_images_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('property_images', function (Blueprint $table) {
+            $table->id('imageId');
+            $table->unsignedBigInteger('propertyId');
+            $table->string('imagePath');
+            $table->boolean('mainImage')->default(false);
+
+            $table->foreign('propertyId')->references('propertyId')->on('properties')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('property_images');
+    }
+};
